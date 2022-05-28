8dc021b8900dfba2bf2925157733722d9d8d9d46
equiman <equiman@users.noreply.github.com>
2022-05-28 10:23:55 -0500

    fix code smells reported by sonar

--- src/helpers/info.js
+++ src/helpers/info.js
@@ -1,6 +1,6 @@
 import { readFile } from 'fs/promises'
 
 export const getSwpmInfo = async () => {
-  const pkg = JSON.parse(await readFile(new URL('../../package.json', import.meta.url)))
-  return pkg
+  const pkg = await readFile(new URL('../../package.json', import.meta.url))
+  return JSON.parse(pkg)
 }
