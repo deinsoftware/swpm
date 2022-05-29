7b33519e9670cb2ad5eebb79ae21a6f2a6669078
equiman <equiman@users.noreply.github.com>
2022-05-28 19:20:31 -0500

    add unit testing

--- src/helpers/cmds.test.js
+++ src/helpers/cmds.test.js
@@ -2,12 +2,12 @@ import { describe, test, expect } from 'vitest'
 import { getCommandResult } from './cmds.js'
 
 describe('getCommandResult()', () => {
-  test.only('should return false when can run the command', () => {
+  test('should return false when can run the command', () => {
     const result = getCommandResult()
     expect(result).toBeFalsy()
   })
 
-  test.only('should return the command result', () => {
+  test('should return the command result', () => {
     const cmd = 'echo "a"'
     const print = 'a'
     const result = getCommandResult(cmd)
