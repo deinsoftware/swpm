cd69e3da62a48885fabb9c14a27c59d5732e232a
equiman <equiman@users.noreply.github.com>
2022-06-01 13:52:47 -0500

    fix volta message on info

--- src/flags/info.js
+++ src/flags/info.js
@@ -13,11 +13,18 @@ export const showPackageInformation = async ({ origin, cmd, config, volta }) => 
 
   const { version: swpmVersion } = await getSwpmInfo()
 
-  let message = `
-    ${chalk.bold('using')}: \t${chalk.hex(config.color).bold(cmd)}
-    ${(origin && chalk.bold('origin') + `: ${getOriginIcon(origin)} ${origin}`)}
-    ${(volta && chalk.bold('volta') + `: \t${chalk.yellow('⚡')} detected`)}
-    
+  let message = ''
+  message += `${chalk.bold('using')}: \t${chalk.hex(config.color).bold(cmd)} \n`
+
+  if (origin) {
+    message += `${(chalk.bold('origin') + `: ${getOriginIcon(origin)} ${origin}`)} \n`
+  }
+
+  if (volta) {
+    message += `${(chalk.bold('volta') + `: \t${chalk.yellow('⚡')} detected`)} \n`
+  }
+
+  message += `
     ${chalk.bold('Versions:')}
     ${chalk.hex('#368fb9').bold('s')}${chalk.hex('#4e4e4e').bold('w')}${chalk.hex('#f8ae01').bold('p')}${chalk.hex('#e32e37').bold('m')}: \t${swpmVersion}
     ${chalk.hex('#689e65').bold('Node')}: \t${nodeVersion?.replace(/v/, '')}
@@ -25,6 +32,7 @@ export const showPackageInformation = async ({ origin, cmd, config, volta }) => 
   `
   if (!isInstalled) { // TODO: different message if npm was not found
     message += `
+
     Install ${chalk.hex(config.color).bold(cmd)} with ${chalk.blue.bold(`npm install ${cmd} --global`)} command
     Visit ${chalk.blue.bold(config.url)} for more information
     `
