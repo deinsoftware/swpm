549bd5722cb40dfa8b93323f74c34f1752384de1
equiman <equiman@users.noreply.github.com>
2022-06-01 11:17:20 -0500

    fix run with volta

--- src/flags/info.js
+++ src/flags/info.js
@@ -2,14 +2,14 @@ import { exit } from 'node:process'
 import { stripIndents } from 'common-tags'
 import chalk from 'chalk'
 import { getCommandResult } from '../helpers/cmds.js'
-import { getOriginIcon } from '../helpers/get.js'
+import { getOriginIcon } from '../helpers/icons.js'
 import { getSwpmInfo } from '../helpers/info.js'
 
 export const showPackageInformation = async ({ origin, cmd, config, volta }) => {
-  const nodeVersion = getCommandResult('node --version').replace(/v/, '')
+  const nodeVersion = getCommandResult('node --version', volta)
 
-  const isInstalled = !!getCommandResult(`command -v ${cmd}`)
-  const packageVersion = isInstalled ? getCommandResult(`${cmd} --version`) : 'not found'
+  const isInstalled = !!(await getCommandResult(`command -v ${cmd}`))
+  const packageVersion = isInstalled ? await getCommandResult(`${cmd} --version`, volta) : 'not found'
 
   const { version: swpmVersion } = await getSwpmInfo()
 
@@ -20,7 +20,7 @@ export const showPackageInformation = async ({ origin, cmd, config, volta }) => 
     
     ${chalk.bold('Versions:')}
     ${chalk.hex('#368fb9').bold('s')}${chalk.hex('#4e4e4e').bold('w')}${chalk.hex('#f8ae01').bold('p')}${chalk.hex('#e32e37').bold('m')}: \t${swpmVersion}
-    ${chalk.hex('#689e65').bold('Node')}: \t${nodeVersion} 
+    ${chalk.hex('#689e65').bold('Node')}: \t${nodeVersion?.replace(/v/, '')}
     ${chalk.hex(config.color).bold(cmd)}: \t${packageVersion}
   `
   if (!isInstalled) { // TODO: different message if npm was not found
