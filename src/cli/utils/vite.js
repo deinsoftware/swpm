f58f6761016c9d4866d9e82fb5294c084583e020
equiman <equiman@users.noreply.github.com>
2022-09-30 11:29:15 -0500

    update TODO notes

--- src/cli/utils/vite.js
+++ src/cli/utils/vite.js
@@ -1,7 +1,8 @@
 export const vite = () => {
-  // TODO
-//   # npm 6.x
-// npm create vite@latest my-vue-app --template vue
+  // NOTE: vite has differences between versions
+
+  //   # npm 6.x
+  // npm create vite@latest my-vue-app --template vue
 
   // # npm 7+, extra double-dash is needed:
   // npm create vite@latest my-vue-app -- --template vue
@@ -9,6 +10,6 @@ export const vite = () => {
   // # yarn
   // yarn create vite my-vue-app --template vue
 
-// # pnpm
-// pnpm create vite my-vue-app -- --template vue
+  // # pnpm
+  // pnpm create vite my-vue-app -- --template vue
 }
