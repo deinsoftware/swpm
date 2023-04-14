04b65afe1ff82f89cbb6894b124b063d5c779575
equiman <equiman@users.noreply.github.com>
2023-04-14 00:39:37 -0500

    format fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
-    reporters: ["verbose"],
+    reporters: ['verbose'],
     coverage: {
       all: true,
-      reporter: ["text", "html", "lcov"],
+      reporter: ['text', 'html', 'lcov'],
       statements: 22,
       branches: 48,
       functions: 27,
-      lines: 22,
-    },
-  },
-});
+      lines: 22
+    }
+  }
+})
