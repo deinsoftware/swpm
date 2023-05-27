af3eb74a5b2fe301285c4337bf1a4301bde05532
equiman <equiman@users.noreply.github.com>
2023-05-27 17:49:18 -0500

    fix delete modules path

--- src/helpers/files.js
+++ src/helpers/files.js
@@ -14,6 +14,15 @@ export const fileExists = async (path) => {
   }
 }
 
+export const pathExists = async (path) => {
+  try {
+    await fs.access(path)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
 export const getPackageJson = async (fileName = packageName) => {
   try {
     const closestPackageJsonPath = await findUp(fileName)
