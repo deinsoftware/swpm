acfed3bfb37cc2d6b9a4645a97e583e8dd9e6fd1
equiman <equiman@users.noreply.github.com>
2023-06-06 17:53:09 -0500

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,35 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
+
+const include = [
+  'src/**/*',
+]
+
+const exclude = [
+  ...configDefaults.exclude,
+  'src/alias/*',
+  'test{,s}/**',
+  'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+  '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}'
+]
 
 export default defineConfig({
   test: {
     globals: true,
     reporters: ['verbose'],
+    include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: [
+      ...configDefaults.exclude,
+      '**/test.{js,cjs,mjs,ts,tsx,jsx}',
+    ],
     coverage: {
       all: true,
       reporter: ['text', 'html', 'lcov'],
-      statements: 27,
-      branches: 53,
-      functions: 28,
-      lines: 27,
+      include: [...include],
+      exclude: [...exclude],
+      statements: 30,
+      branches: 68,
+      functions: 42,
+      lines: 30,
     }
   }
 })
