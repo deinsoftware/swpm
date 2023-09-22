ba9fbd61593979b86234eb1bc37ca2e20d76a102
equiman <equiman@users.noreply.github.com>
2023-09-21 20:44:47 -0500

    fix linter configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,18 @@
 module.exports = {
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ['@typescript-eslint'],
+  extends: ['standard', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   env: {
     es2021: true,
     node: true
   },
-  parser: "@typescript-eslint/parser",
-  extends: ['standard'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type']
   }
+
 }
