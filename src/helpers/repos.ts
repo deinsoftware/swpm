6d1c0e79607e36fae211deb8e5a5add5bbd39c8f
equiman <equiman@users.noreply.github.com>
2023-10-19 09:32:23 -0500

    fix: :bug: fix open repo with ssh
    fix url recognition when local repo was configured using ssh

--- src/helpers/repos.ts
+++ src/helpers/repos.ts
@@ -11,19 +11,26 @@ const gitCurrentBranch = () => {
   return getCommandResult({ command: 'git branch --show-current' })
 }
 
+const gitOrigin = () => {
+  return getCommandResult({ command: 'git config remote.origin.url' })
+}
+
 export const getReposStatus = async () => {
-  const origin = getCommandResult({ command: 'git config remote.origin.url' })
-  const host = new URL(origin)?.hostname
-  const provider = host?.split('.').at(0) as Providers
+  const origin = gitOrigin()
+  const current = gitCurrentBranch()
 
-  const config = await getProviderConfiguration({ id: provider })
-
-  let url = origin.replace(`git@${origin}`, `https://${origin}/`)
+  let url = origin
+  if (url.startsWith('git@')) {
+    url = url.split(':').join('/')
+    url = url.replace('git@', 'https://')
+  }
   if (url.endsWith('.git')) {
     url = url.slice(0, -4)
   }
 
-  const current = gitCurrentBranch()
+  const { hostname } = new URL(url) ?? {}
+  const provider = hostname?.split('.').at(0) as Providers
+  const config = await getProviderConfiguration({ id: provider })
 
   const repository: Repository = {
     ...config,
